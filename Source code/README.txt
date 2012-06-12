f92f43c782f63c03fb8bece8060d1b6131dd27b8
Jeff Brusse <be_the_trade@yahoo.com>
2012-06-12 12:05:25 -0700

    .gitignore Workaround: Build the source code somewhere else.

--- Source code/README.txt
+++
@@ -1,6 +0,0 @@
-Delete the following files if you've forked this repository:
-
-.metadata
-Everything but "Source Files" in the "FSG Source" Folder
-
-Note: If you would like to contribute a .gitignore file so this "prep work" can be abolished, please do.
---
+++ Source code/RealmsOfRainfur.cpp
@@ -0,0 +1,18 @@
+/*
+ * RealmsOfRainfur.cpp
+ *
+ *  Created on: Jun 12, 2012
+ *      Author: Elusivehawk
+ */
+
+#include "RealmsOfRainfur.h"
+
+RealmsOfRainfur::RealmsOfRainfur() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RealmsOfRainfur::~RealmsOfRainfur() {
+	// TODO Auto-generated destructor stub
+}
+

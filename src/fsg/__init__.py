396333e15e0ba5364488200f10fa77437edcfeb6
ObsequiousNewt <obsequiousnewt@terebithia>
2012-10-21 16:19:37 -0500

    Added a basic GUI

---
+++ src/fsg/__init__.py
@@ -0,0 +1,11 @@
+'''
+This is not the documentation you're looking for.
+
+Look in the Documentation folder for documentation.
+
+Move along!
+'''
+import gui
+'''I don't know if this file will be run or if another file
+will, but I put this here anyway.'''
+gui.start()

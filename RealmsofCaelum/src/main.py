bf021d288db09a772c0b12b3869944c57ba8878f
Jeff Brusse <be_the_trade@yahoo.com>
2012-10-22 23:00:33 -0700

    This is going to be awkward...

--- RealmsofCaelum/src/main.py
+++ RealmsofCaelum/src/main.py
@@ -1,25 +1,23 @@
+
 '''
 Created on Oct 22, 2012
 
-@author: ObsequiousNewt
+@author: Elusivehawk and ObsequiousNewt
 '''
 
-import direct.directbase.DirectStart
 import direct.gui.DirectGui as dg
-import panda3d.core as p3
+import panda3d.os as p3
+from direct.showbase.ShowBase import ShowBase
 
-class window:
-	def __init__(self):
-		self.title=dg.OnscreenText(text = "Realms of C\xc3\x86lum",
-								   style = 1,
-								   fg = (1, 1, 1, 1),
-								   pos = (0.8,-0.95),
-								   scale = .07)
-		base.setBackgroundColor(0, 0, 0)
-		base.disableMouse()
-		camera.setPos(0,0,45)
-		camera.setHpr(0,-90,0)
-		
-win=window()
+class main(ShowBase):
+ 
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.title=dg.OnscreenText(text = "Realms of C\xc3\x86lum",
+                                   style = 1,
+                                   fg = (1, 1, 1, 1),
+                                   pos = (0.8,-0.95),
+                                   scale = .07)
 
-run()
+app = main()
+app.run()

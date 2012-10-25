608dcb66aac3a0f14c9aa8658c6246598b3a9b21
ObsequiousNewt <obsequiousnewt@gmail.com>
2012-10-25 16:55:15 -0500

    Various æsthetic edits

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -2,16 +2,21 @@
 
 import direct.gui.DirectGui as dg
 
-class base:
+buttonsize = 0.1
 
+class base:
+        '''Basic GUI superclass. Inherit this!'''
 	def __init__(self,win):
 		self.win = win
-		self.frame = dg.DirectFrame(frameSize = (-1.2,-0.6,0.9,-0.2))
+		self.frame = dg.DirectFrame(frameSize = (-1,1,-1,1),
+                                            color=(0,0,0,0))
 
 	def hide(self):
+                '''Called to hide the GUI.'''
 		self.frame.hide()
 
 	def show(self):
+                '''Called to show the GUI.'''
 		self.frame.show()
 
 class main(base):
@@ -20,16 +25,21 @@ class main(base):
 		base.__init__(self,win)
 		self.new = dg.DirectButton(parent = self.frame,
 					   text = ("New Game"), #TODO! Let's add support for languages early, so we don't have to deal with that later when it'll be harder.
-					   pos = (-1,0,0.8),
-					   scale = .05,
+					   pos = (0,0,0.8),
+					   scale = buttonsize,
 					   command = win.new_game)
 		self.options = dg.DirectButton(parent = self.frame,
 					       text = ("Options"),
-					       pos = (-1,0,0.6),
-					       scale = .05,
+					       pos = (0,0,0.6),
+					       scale = buttonsize,
 					       command = win.openoptions)
 
 class options(base):
 
 	def __init__(self,win):
 		base.__init__(self,win)
+		self.back = dg.DirectButton(parent = self.frame,
+                                            text = ("Back"),
+                                            pos = (0,0,-0.8),
+                                            scale = buttonsize,
+                                            command = win.openmain)
--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ import panda3d.core as p3
 import gui
 
 class window(ds.ShowBase):
+	'''Main window class, instantiated upon start of game. This class
+	starts the menu.'''
 	def __init__(self):
 		ds.ShowBase.__init__(self)
 		base.setBackgroundColor(0, 0, 0)
@@ -27,20 +29,23 @@ class window(ds.ShowBase):
 		self.openmain()
 	
 	def hide_menus(self):
+		'''Hides any active menus. Called before switching menus.'''
 		self.menu_main.hide()
 		self.menu_options.hide()
 		
 	def openmain(self):
+		'''Opens the main menu.'''
 		self.hide_menus()
 		self.menu_main.show()
 
 	def openoptions(self):
+		'''Opens the options menu.'''
 		self.hide_menus()
 		self.menu_options.show()
 
 	def new_game(self):
+		'''Opens the new-game menu.'''
 		pass #We will get to this... *much* later.
-        
 
 win=window()
 win.run()

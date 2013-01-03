19a420e175841f39a2c378f0bd9cf7edbc5e8cd6
Jeff Brusse <be_the_trade@yahoo.com>
2013-01-02 17:17:36 -0800

    *Insert pun here*

---
+++ java/eclipse/RoC/src/Start.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * 
+ * 
+ * @author atrain99, briman0094, Elusivehawk, Kovu, ObsequiousNewt
+ */
+public class Start
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
